4e12bbe0ce8f153a96f8e0f97f79907b6388bdb7
RukhsarBanu247 <banurukhsar247@gmail.com>
2025-03-01 16:27:08 +0530

    commit1

---
+++ src/test/java/Sample/pack/DTest.java
@@ -0,0 +1,10 @@
+package Sample.pack;
+
+public class DTest
+{
+	public static void main(String[] args) {
+		
+		System.out.println("hii github");
+	}
+
+}
---
+++ src/test/java/Sample/pack/ETest.java
@@ -0,0 +1,11 @@
+package Sample.pack;
+
+public class ETest
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("bye Github");
+		
+	}
+
+}
